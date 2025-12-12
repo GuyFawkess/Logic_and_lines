8315bc7a138eff32d5acfa3ea8c74e07cd644042
Jason the noob <jason-ollivierre@hotmail.com>
2025-12-12 17:26:10 +0000

    video hero puesto, tambien hemos descubierto que se puede cambiar el color del video con solo codigo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  // ...other config
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      transitionDuration: {
-        '1500': '1500ms',
-        '2000': '2000ms',
-        '2500': '2500ms',
-        '3000': '3000ms',
-      }
-    }
-  }
+      colors: {
+        'dark-blue': '#050e1b',
+        'medium-blue': '#0e356a',
+        'bright-blue': '#1057af',
+        'cyan': '#3e98da',
+        'accent-red': '#9f2239',
+      },
+      fontFamily: {
+        'display': ['"Playfair Display"', 'serif'],
+        'sans': ['"Host Grotesk"', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
