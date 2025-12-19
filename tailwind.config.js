475d79673435aa4da950fd7aab7829f268833a88
Jason the noob <jason-ollivierre@hotmail.com>
2025-12-19 17:46:40 +0000

    Carrosell the social links for the footer almost done

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    extend: {
+      animation: {
+        'skew-scroll': 'skew-scroll 20s linear infinite',
+      },
+      keyframes: {
+        'skew-scroll': {
+          '0%': {
+            transform: 'rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(0)',
+          },
+          '100%': {
+            transform:
+              'rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(-100%)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
